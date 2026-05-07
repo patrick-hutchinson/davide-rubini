17ce07cf78db81c10ab84e7fa0863869967444dd
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-05-07 12:22:36 +0200

    Limit grid links to image and title

--- app/src/app/projects/components/GridView.jsx
+++ app/src/app/projects/components/GridView.jsx
@@ -9,21 +9,22 @@ const GridView = ({ projects }) => {
     <section className={styles.gridViewSection}>
       <div className={styles.gridView}>
         {projects.map((project, index) => (
-          <AnimationLink
-            key={project._id}
-            className={styles.gridCard}
-            link={`/projects/${project.slug.current}`}
-            preloadSrc={project.coverMedia?.medium?.url}
-          >
-            <strong className={styles.gridCardTitle}>{project.title}</strong>
-            <Medium
-              className={styles.gridCardMedia}
-              medium={project.coverMedia?.medium}
-              sizes="(max-width: 47.99rem) calc((100vw - 24px) / 2), calc((100vw - 40px) / 4)"
-              quality={75}
-              eager={index < eagerPreviewCount}
-            />
-          </AnimationLink>
+          <div key={project._id} className={styles.gridCard}>
+            <AnimationLink
+              className={styles.gridCardLink}
+              link={`/projects/${project.slug.current}`}
+              preloadSrc={project.coverMedia?.medium?.url}
+            >
+              <strong className={styles.gridCardTitle}>{project.title}</strong>
+              <Medium
+                className={styles.gridCardMedia}
+                medium={project.coverMedia?.medium}
+                sizes="(max-width: 47.99rem) calc((100vw - 24px) / 2), calc((100vw - 40px) / 4)"
+                quality={75}
+                eager={index < eagerPreviewCount}
+              />
+            </AnimationLink>
+          </div>
         ))}
       </div>
     </section>
